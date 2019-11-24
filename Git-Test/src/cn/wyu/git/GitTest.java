6ea66602109712a5adae0ef7eaebda5196fd117c
Tom <18027761141@163.com>
2019-11-24 12:08:39 +0800

    add test method.

--- Git-Test/src/cn/wyu/git/GitTest.java
+++ Git-Test/src/cn/wyu/git/GitTest.java
@@ -9,4 +9,8 @@ public class GitTest {
         System.out.println("Hello my first git");
         System.out.println("Hello my first git");
     }
+    public void test(int i) {
+        System.out.println("my first method "+i);
+    }
+
 }
