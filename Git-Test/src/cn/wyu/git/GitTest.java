74069d1a2bef9d61b6b84e7020dee53a71730970
Tom <18027761141@163.com>
2019-11-24 12:19:01 +0800

    update GitTest.

--- Git-Test/src/cn/wyu/git/GitTest.java
+++ Git-Test/src/cn/wyu/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("Hello my first git");
         System.out.println("Hello my first git");
         System.out.println("Hello my first git");
+        System.out.println("修改了Git Test 文件");
     }
     public void test(int i) {
         System.out.println("my first method "+i);
