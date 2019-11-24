9ae29e5d0475637f12927688bd9153c65b5efb6f
Tom <18027761141@163.com>
2019-11-24 12:06:44 +0800

    add first idea project to github.

---
+++ Git-Test/src/cn/wyu/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.wyu.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello my first git");
+        System.out.println("Hello my first git");
+        System.out.println("Hello my first git");
+        System.out.println("Hello my first git");
+        System.out.println("Hello my first git");
+        System.out.println("Hello my first git");
+    }
+}
